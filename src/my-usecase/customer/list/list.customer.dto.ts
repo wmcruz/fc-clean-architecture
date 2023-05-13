897c1980425b28c44524e73528d5a6464da4b86e
Wellington Cruz <wellington.max1@gmail.com>
2023-05-13 20:53:51 -0300

    Create usecases of customer.

---
+++ src/my-usecase/customer/list/list.customer.dto.ts
@@ -0,0 +1,18 @@
+export interface InputListCustomerDto {
+
+}
+
+type Customer = {
+    id: string;
+    name: string;
+    address: {
+        street: string;
+        number: number;
+        zip: string;
+        city: string;
+    };
+};
+
+export interface OutputListCustomerDto {
+    customers: Customer[];
+}
